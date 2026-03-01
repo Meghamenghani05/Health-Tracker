60190ac3d03372e824bbb0f72f63c0ad4ce58871
[meghamenghani] <[meghamenghani401@gmail.com]>
2026-03-01 17:50:14 +0530

    changes

--- backend/routes/ai.routes.js
+++ backend/routes/ai.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const auth = require("../middleware/auth.middleware");
 const { getAIInsights } = require("../controllers/ai.controllers");
 
-router.get("/ai-insights", getAIInsights);
+router.get("/ai-insights",auth, getAIInsights);
 
 module.exports = router;
